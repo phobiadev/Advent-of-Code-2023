e6a0b9486a7bed6951de7de95793fc3460866ed2
Joseph Preston <joseph@Josephs-MacBook-Air.local>
2023-12-06 20:28:16 +0000

    removed redundant .index() from day-1/part-2 by using enumerate()

--- day-1/part-2.py
+++ day-1/part-2.py
@@ -8,20 +8,20 @@ def firstDigit(line):
         char = line[i]
         if char.isnumeric():
             return char
-        for num in nums:
+        for j, num in enumerate(nums):
             if (i + len(num)) < len(line):
                 if line[i:i+len(num)] == num:
-                    return str((nums.index(num) + 1))
+                    return str(j + 1)
         
 def lastDigit(line):
     for i in range(len(line)-1,-1,-1):
         char = line[i]
         if char.isnumeric():
             return char
-        for num in nums:
+        for j, num in enumerate(nums):
             if i >= len(num):
                 if line[i-len(num)+1:i+1] == num:
-                    return str((nums.index(num) + 1))
+                    return str(j + 1)
 
 
 for line in lines:
