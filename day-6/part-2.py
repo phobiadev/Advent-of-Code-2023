e9adad1cdeaad5b7a2cba9ea37010f7e7bda6aef
Joseph Preston <joseph@Josephs-MacBook-Air.local>
2023-12-06 15:19:28 +0000

    fixed typo

--- day-6/part-2.py
+++ day-6/part-2.py
@@ -8,7 +8,7 @@ record = int("".join(filter(lambda x: x.isdigit(),lines[1])))
 # small adjustment to make sure only distances greater than the record are counted    
 sqrtDiscriminant = (time**2 - 4*(record+0.01))**(1/2)
 root1 = (time + sqrtDiscriminant)//2
-root2 = (time - sqrtDiscriminant)//2 # should be +1 but this cancels late
+root2 = (time - sqrtDiscriminant)//2 # should be +1 but this cancels later
 total = root1 - root2
 
 print(int(total))
