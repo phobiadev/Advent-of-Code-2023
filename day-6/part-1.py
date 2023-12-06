6077700a96603c814669d0beac3a1729087caecc
Joseph Preston <joseph@Josephs-MacBook-Air.local>
2023-12-06 15:18:26 +0000

    optimised day-6/part1&2

--- day-6/part-1.py
+++ day-6/part-1.py
@@ -9,10 +9,10 @@ total = 1
 
 for i in range(len(times)):
     time, record = times[i], records[i]
-    sqrtDiscriminant = (time**2 - 4*record)**(1/2)
-    # small adjustments in case roots are integers
-    root1 = (time + sqrtDiscriminant)/2 - 0.01
-    root2 = (time - sqrtDiscriminant)/2 + 0.01
-    total *= math.floor(root1) - math.ceil(root2) + 1
+    # small adjustment to make sure only distances greater than the record are counted
+    sqrtDiscriminant = (time**2 - 4*(record+0.01))**(1/2)
+    root1 = (time + sqrtDiscriminant)//2
+    root2 = (time - sqrtDiscriminant)//2 # should be +1 but this cancels later
+    total *= root1 - root2
 
-print(total)
+print(int(total))
--- day-6/part-2.py
+++ day-6/part-2.py
@@ -4,12 +4,11 @@ lines = open("input.txt").read().splitlines()
 
 time = int("".join(filter(lambda x: x.isdigit(),lines[0])))
 record = int("".join(filter(lambda x: x.isdigit(),lines[1])))
-    
-sqrtDiscriminant = (time**2 - 4*record)**(1/2)
-# small adjustment in case roots are integers
-root1 = (time + sqrtDiscriminant)/2 - 0.01
-root2 = (time - sqrtDiscriminant)/2 + 0.01
 
-total = math.floor(root1) - math.ceil(root2) + 1
+# small adjustment to make sure only distances greater than the record are counted    
+sqrtDiscriminant = (time**2 - 4*(record+0.01))**(1/2)
+root1 = (time + sqrtDiscriminant)//2
+root2 = (time - sqrtDiscriminant)//2 # should be +1 but this cancels late
+total = root1 - root2
 
-print(total)
+print(int(total))
