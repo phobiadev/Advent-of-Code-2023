2a33de67668fe42f3a1c0ec777d009f33be5a942
Joseph Preston <joseph@Josephs-MacBook-Air.local>
2023-12-06 19:18:58 +0000

    made day-6/part1&2 solutions more concise

--- day-6/part-1.py
+++ day-6/part-1.py
@@ -11,8 +11,7 @@ for i in range(len(times)):
     time, record = times[i], records[i]
     # small adjustment to make sure only distances greater than the record are counted
     sqrtDiscriminant = (time**2 - 4*(record+0.01))**(1/2)
-    root1 = (time + sqrtDiscriminant)//2
-    root2 = (time - sqrtDiscriminant)//2 # should be +1 but this cancels later
-    total *= root1 - root2
+    # root being subtracted should be 1 larger (ceil not floor) but this cancels out with making the range inclusive by adding 1
+    total *= (time + sqrtDiscriminant)//2 - (time - sqrtDiscriminant)//2
 
 print(int(total))
--- day-6/part-2.py
+++ day-6/part-2.py
@@ -7,8 +7,7 @@ record = int("".join(filter(lambda x: x.isdigit(),lines[1])))
 
 # small adjustment to make sure only distances greater than the record are counted    
 sqrtDiscriminant = (time**2 - 4*(record+0.01))**(1/2)
-root1 = (time + sqrtDiscriminant)//2
-root2 = (time - sqrtDiscriminant)//2 # should be +1 but this cancels later
-total = root1 - root2
+# root being subtracted should be 1 larger (ceil not floor) but this cancels out with making the range inclusive by adding 1
+total = (time + sqrtDiscriminant)//2 - (time - sqrtDiscriminant)//2
 
 print(int(total))
