d59ab01365bbae8fedb5c082913612fe95b223d7
Joseph Preston <joseph@phobia.dev>
2023-12-09 09:28:00 +0000

    made day-9/part-1&2 solutions more concise

--- day-9/part-1.py
+++ day-9/part-1.py
@@ -3,14 +3,12 @@ from itertools import pairwise
 lines = open("input.txt")
 lines = [[int(x) for x in line.split(" ")] for line in lines]
     
-total = 0
+total = sum(line[-1] for line in lines)
 
 for line in lines:
     differences = line
-    prediction = line[-1]
     while len(set(differences)) != 1:
         differences = [y-x for (x,y) in pairwise(differences)]
-        prediction += differences[-1]
-    total += prediction
+        total += differences[-1]
     
 print(total)
