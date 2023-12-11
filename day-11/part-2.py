70ea83a0c91e21390ad14b667f6e8061f57c7ddd
Joseph Preston <joseph@phobia.dev>
2023-12-11 23:27:14 +0000

    made day 11/part 2 solution more concise

--- day-11/part-2.py
+++ day-11/part-2.py
@@ -9,18 +9,14 @@ expansionColumns = [i for i in range(width) if len(set([line[i] for line in line
 
 galaxies = [[x, y] for y in range(height) for x in range(width) if lines[y][x] == "#"]
 
-def between(a,b):
-    return range(min(a,b),max(a,b))
-
 total = 0
-
 for g1 in galaxies:
     for g2 in galaxies:
-        rowIndexes = between(g1[1],g2[1])
+        rowIndexes = range(min(g1[1],g2[1]),max(g1[1],g2[1]))
         expandedRows = len([i for i in rowIndexes if i in expansionRows])
         total += expandedRows * expansion + len(rowIndexes) - expandedRows
         
-        columnIndexes = between(g1[0],g2[0])
+        columnIndexes = range(min(g1[0],g2[0]),max(g1[0],g2[0]))
         expandedColumns = len([i for i in columnIndexes if i in expansionColumns])
         total += expandedColumns * expansion + len(columnIndexes) - expandedColumns
             
